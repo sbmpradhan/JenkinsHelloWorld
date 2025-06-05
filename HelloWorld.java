160176ae54a567bb3c4c641a66ab7aaac2373c08
sbmpradhan <sbmpradhan@gmail.com>
2025-06-05 10:03:43 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-	System.out.println("..**** HelloWorld & welcome to Jenkins ****..");
+	System.out.println("..**** HelloWorld & welcome to Jenkins Github scm polling ****..");
 	}
 }
